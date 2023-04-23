3db92f1b6966079454d3148ff6e2396073910bcc
Chandler Farley <chandlerfarley@live.com>
2023-04-23 01:08:49 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {return false}
+  }
+  return true
 }
 
 /* 
@@ -15,6 +20,12 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("tattarrattat"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("Antidisestablishmentarianism"));
 
   console.log("");
 
